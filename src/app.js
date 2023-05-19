c5085bf514171822f148edc29a6226b30524e736
Bernardo Marquez <marquez.cmbh@gmail.com>
2023-05-19 15:46:06 -0300

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import express from "express";
+import cors from "cors";
+
+const server = express();
+server.use(cors());
+server.use(express.json());
+
+const port = process.env.PORT || 5000;
+server.listen(port, () => console.log("Server is running on port: " + port));
